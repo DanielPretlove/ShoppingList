33917964d6c7d12e02bfb14ffd631c58db1daa44
Daniel Pretlove <danielpretlove5@gmail.com>
2021-09-03 01:52:12 +1000

    first commit

---
+++ ShoppingCart/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using ShoppingCart.Models;
+using ShoppingCart.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class ItemController : Controller
+    {
+        ECartDBEntities objECartEntities;
+
+        public ItemController()
+        {
+            objECartEntities = new ECartDBEntities();
+        }
+
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objECartEntities.Categories
+                select new SelectListItem() 
+                {
+                    Text = objCat.CategoryName,
+                    Value = objCat.CategoryID.ToString(),
+                    Selected = true
+                });
+            return View(objItemViewModel);
+        }
+    }
+}
